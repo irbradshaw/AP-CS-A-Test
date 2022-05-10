c99cf07da5320a4f06f44569e8845056aa2e43ca
s-IBRADSHAW <s-IBRADSHAW@484E293865.lwsd.k12wa.org>
2022-05-10 13:45:24 -0700

    Testing to make sure i know what I am doing.

---
+++ src/helloTest.java
@@ -0,0 +1,10 @@
+
+public class helloTest {
+
+	public static void main(String[] args) {
+		System.out.println("Yeehaw je suis un robot.");
+		System.out.println("According to the CDC, ADHD is one of the most common neurodevelopmental disorders of childhood.");
+		System.out.println("It is usually first diagnosed in childhood and lasts into adulthood.");
+		System.out.println("This is just a generalization though as many people with ADHD say that it’s very common to not be diagnosed until later in life, meaning past 19.");
+	}
+}
